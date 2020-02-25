5024650e596d712b5e3f8e59819827c8efc2a4f3
amitavainfluxiq <amitava.involutiontech@gmail.com>
2020-02-25 15:32:10 +0530

    amitava MetaService

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetaService } from '@ngx-meta/core';
 
 @Component({
   selector: 'app-team',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  constructor() { window.scrollTo(500, 0);}
+  constructor(private readonly meta: MetaService,) { window.scrollTo(500, 0);
+    this.meta.setTitle('DataMed Unlimited - Meet The Team');
+    this.meta.setTag('og:description', 'Meet the team that has enabled us to meet the Gold Standard in the ANS Testing industry and move beyond. Extremely qualified and highly experienced, our team of professional experts has helped us become a pioneer in the medical testing industry.');
+    this.meta.setTag('twitter:description', 'Meet the team that has enabled us to meet the Gold Standard in the ANS Testing industry and move beyond. Extremely qualified and highly experienced, our team of professional experts has helped us become a pioneer in the medical testing industry.');
+
+    this.meta.setTag('og:keyword', 'DataMed Unlimited Team, Team DataMed Unlimited, DataMed Unlimited Team Members, Team Members Of DataMed Unlimited');
+    this.meta.setTag('twitter:keyword', 'DataMed Unlimited Team, Team DataMed Unlimited, DataMed Unlimited Team Members, Team Members Of DataMed Unlimited');
+
+    this.meta.setTag('og:title', 'DataMed Unlimited - Meet The Team');
+    this.meta.setTag('twitter:title', 'DataMed Unlimited - Meet The Team');
+    this.meta.setTag('og:type', 'website');
+    this.meta.setTag('og:image', 'https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/logo.png');
+    this.meta.setTag('twitter:image', 'https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/logo.png');
+  
+  }
 
   ngOnInit() {
   }
