ffad681a94da1996b07d8065908863b2fa6f0331
amitavainfluxiq <amitava.involutiontech@gmail.com>
2020-02-25 13:21:28 +0530

    25-02-2020

--- src/app/allergy-testing/allergy-testing.component.ts
+++ src/app/allergy-testing/allergy-testing.component.ts
@@ -24,20 +24,20 @@ export class AllergyTestingComponent implements OnInit {
     console.warn(this.reportImg);
   }
 
-
+ 
   reportImg= [
-    {id:"1", image:"../../../assets/images/REPORT_img1_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img1.jpg"},
-    {id:"2", image:"../../../assets/images/REPORT_img2_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img2.jpg"},
-    {id:"3", image:"../../../assets/images/REPORT_img3_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img3.jpg"},
-    {id:"4", image:"../../../assets/images/REPORT_img4_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img4.jpg"},
-    {id:"5", image:"../../../assets/images/REPORT_img5_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img5.jpg"},
-    {id:"6", image:"../../../assets/images/REPORT_img6_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img6.jpg"},
-    {id:"7", image:"../../../assets/images/REPORT_img7_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img7.jpg"},
-    {id:"8", image:"../../../assets/images/REPORT_img8_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img8.jpg"},
-    {id:"9", image:"../../../assets/images/REPORT_img9_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img9.jpg"},
-    {id:"10", image:"../../../assets/images/REPORT_img10_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img10.jpg"},
-    {id:"11", image:"../../../assets/images/REPORT_img11_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img11.jpg"},
-    {id:"12", image:"../../../assets/images/REPORT_img12_sm.jpg", subtitle:"REPORT Form", bigImage:"../../../assets/images/REPORT_img12.jpg"},
+    {id:"1", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img1_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img1.jpg"},
+    {id:"2", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img2_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img2.jpg"},
+    {id:"3", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img3_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img3.jpg"},
+    {id:"4", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img4_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img4.jpg"},
+    {id:"5", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img5_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img5.jpg"},
+    {id:"6", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img6_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img6.jpg"},
+    {id:"7", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img7_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img7.jpg"},
+    {id:"8", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img8_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img8.jpg"},
+    {id:"9", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img9_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img9.jpg"},
+    {id:"10", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img10_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img10.jpg"},
+    {id:"11", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img11_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img11.jpg"},
+    {id:"12", image:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img12_sm.jpg", subtitle:"REPORT Form", bigImage:"https://all-frontend-assets.s3.amazonaws.com/datamed-unlimited/assets/images/REPORT_img12.jpg"},
 
 ]
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { environment } from '../environments/environment';
 import { isPlatformBrowser, DOCUMENT } from '@angular/common';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+  
 export class AppComponent implements OnInit {
   title = 'datamed-serverless';
+  loading = false;
+
+
+
+
+
 public ngOnInit(): void {
     if (!isPlatformBrowser(this.platformId)) {
         const bases = this.document.getElementsByTagName('base');
@@ -20,5 +37,31 @@ public ngOnInit(): void {
 }
 
 
- constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any) {}
+ constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any, private router: Router) {
+
+
+  this.router.events.subscribe((event: Event) => {
+    switch (true) {
+      case event instanceof NavigationStart: {
+        this.loading = true;
+        break;
+      }
+
+      case event instanceof NavigationEnd:
+      case event instanceof NavigationCancel:
+      case event instanceof NavigationError: {
+        this.loading = false;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  });
+  
+ }
+
+
+ 
 }
+ 
